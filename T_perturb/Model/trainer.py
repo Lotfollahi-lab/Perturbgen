fix bugs in validation step w/o random masking

T_perturb/Model/trainer.py
@@ -168,13 +168,44 @@ class Petratrainer(LightningModule):
         num_steps = len(self.time_steps)
         cls_positions = np.arange(0, num_steps * interval, interval)
 
-        outputs = self.transformer(
-            src_input_id=batch['src_input_ids'],
-            tgt_input_id_dict=tgt_input_id_dict,
-            original_lens=batch['src_length'],
-            generate=self.generate,
-            cls_positions=cls_positions,
-        )
+        if self.training:
+            outputs = self.transformer(
+                src_input_id=batch['src_input_ids'],
+                tgt_input_id_dict=tgt_input_id_dict,
+                original_lens=batch['src_length'],
+                generate=self.generate,
+                cls_positions=cls_positions,
+            )
+        else:
+            dec_embeddings = []
+            logits = []
+            labels = []
+            for i in self.time_steps:
+                tgt_input_id_dict_ = {
+                    f'tgt_input_id_t{i}': tgt_input_id_dict[f'tgt_input_id_t{i}']
+                }
+                outputs = self.transformer(
+                    src_input_id=batch['src_input_ids'],
+                    tgt_input_id_dict=tgt_input_id_dict_,
+                    original_lens=batch['src_length'],
+                    generate=self.generate,
+                    cls_positions=cls_positions,
+                    test_time_step=i,
+                    return_embeddings=self.return_embeddings,
+                )
+                if self.return_embeddings:
+                    dec_embeddings.append(outputs['dec_embedding'])
+                else:
+                    # validation
+                    logits.append(outputs['logits'])
+                    labels.append(outputs['labels'])
+
+            if self.return_embeddings:
+                outputs['dec_embedding'] = torch.cat(dec_embeddings, dim=1)
+            else:
+                outputs['logits'] = torch.cat(logits, dim=1)
+                outputs['labels'] = torch.cat(labels, dim=1)
+
         return outputs
 
     def configure_optimizers(self):
@@ -268,6 +299,7 @@ class Petratrainer(LightningModule):
     def test_step(self, batch, *args, **kwargs):
         if self.return_embeddings:
             outputs = self.forward(batch)
+            outputs
             for time_step in self.time_steps:
                 cls_position = outputs['cls_positions'][time_step - 1]
                 cls_embeddings = outputs['dec_embedding'][:, cls_position, :]
@@ -840,11 +872,12 @@ class CountDecodertrainer(LightningModule):
         pred_counts = torch.cat(self.val_pred_counts_list)
         pred_delta_counts = torch.cat(self.val_pred_delta_counts_list)
         true_delta_counts = torch.cat(self.val_true_delta_counts_list)
-        cell_type = np.concatenate(self.val_tgt_cell_type_list)
-        cell_population = np.concatenate(self.val_tgt_cell_population_list)
-        tgt_donor = np.concatenate(self.val_tgt_donor_list)
+
         # create adata for mmd and emd
         if self.generate:
+            cell_type = np.concatenate(self.val_tgt_cell_type_list)
+            cell_population = np.concatenate(self.val_tgt_cell_population_list)
+            tgt_donor = np.concatenate(self.val_tgt_donor_list)
             true_counts = true_counts.detach().cpu()
             pred_counts = pred_counts.detach().cpu()
             test_obs = pd.DataFrame(

T_perturb/pp/plt_tokenisation.py
@@ -21,23 +21,33 @@ style.use(
 
 seed_no = 42
 np.random.seed(seed_no)
+if os.getcwd().split('/')[-3] != 'T_perturb':
+    # set working directory to root of repository
+    os.chdir(
+        '/lustre/scratch123/hgi/projects/healthy_imm_expr/'
+        't_generative/T_perturb/T_perturb/pp'
+    )
+    print('Changed working directory to root of repository')
+
 
 # --- Explore tokenised data ---
 # Filter adata for only DEGs
-degs = pd.read_csv(
+# read pickle file
+with open(
     '/lustre/scratch123/hgi/projects/healthy_imm_expr/t_generative/'
-    'generative_modelling_omic_notebooks/'
-    'pp/res/deg/significant_deg_1.5logfc_0.05padj_hvg_5k.csv'
-)
-unique_degs = degs['names'].unique()
-dataset = load_from_disk('./res/dataset/cytoimmgen_tokenised_degs.dataset')
+    'T_perturb/T_perturb/pp/res/token_id_to_genename_hvg.pkl',
+    'rb',
+) as f:
+    tokenid_to_hvg_genename = pickle.load(f)
+unique_hvg_genes = list(tokenid_to_hvg_genename.values())
+dataset = load_from_disk('./res/dataset/cytoimmgen_tokenised_hvg_paired.dataset')
 # extract length of tokenised data
 length = dataset['length']
 # plot histogram of length
 plt.hist(length, bins=100)
-plt.xlabel('DEG/cell')
+plt.xlabel('hvg/cell')
 plt.ylabel('Counts')
-plt.savefig('./res/tokenised_deg/length_histogram.pdf', dpi=300, bbox_inches='tight')
+plt.savefig('./res/tokenised_hvg/length_histogram.pdf', dpi=300, bbox_inches='tight')
 plt.close()
 
 # create pickle file with length
@@ -65,7 +75,7 @@ for i in tqdm(range(len(dataset))):
     ensembl_ids = [swapped_token_dictionary.get(i, None) for i in input_ids_tmp]
     ensembl_ids_list.append(ensembl_ids)
 # load adata
-adata = sc.read_h5ad('./res/h5ad_data/cytoimmgen_tokenisation_degs.h5ad')
+adata = sc.read_h5ad('./res/h5ad_pairing_hvg/cytoimmgen_tokenised_hvg.h5ad')
 # use adata var to map ensembl ids to gene names
 ensembl_id_to_genename = dict(zip(adata.var_names, adata.var['gene_name']))
 gene_name_list = [
@@ -73,63 +83,99 @@ gene_name_list = [
     for ensembl_ids in ensembl_ids_list
 ]
 
-# in gene_name_list if gene name is in unique_degs then append idx to dictionnary
-degs_idx_dict: Dict[str, list] = {}
+# in gene_name_list if gene name is in unique_hvgs then append idx to dictionnary
+hvg_idx_dict: Dict[str, list] = {}
 
-for gene in tqdm(unique_degs):
-    degs_idx_dict[gene] = []
+for gene in tqdm(unique_hvg_genes):
+    hvg_idx_dict[gene] = []
     for gene_name in gene_name_list:
         if gene in gene_name:
             # append index of gene
-            degs_idx_dict[gene].append(gene_name.index(gene))
+            hvg_idx_dict[gene].append(gene_name.index(gene))
         else:
-            degs_idx_dict[gene].append(np.nan)
+            hvg_idx_dict[gene].append(np.nan)
 # save dictionary
-with open('./res/tokenised_deg/degs_tokenisation_overlap.pkl', 'wb') as f:
-    pickle.dump(degs_idx_dict, f)
+with open('./res/tokenised_hvg/hvgs_tokenisation_overlap.pkl', 'wb') as f:
+    pickle.dump(hvg_idx_dict, f)
+
 # create dataframe
-degs_idx_df = pd.DataFrame.from_dict(degs_idx_dict)
-degs_idx_df['Time_point'] = dataset['Time_point']
-degs_idx_df['Time_point'] = pd.Categorical(
-    degs_idx_df['Time_point'], ['0h', '16h', '40h', '5d']
+hvg_idx_df = pd.DataFrame.from_dict(hvg_idx_dict)
+hvg_idx_df['Time_point'] = dataset['Time_point']
+hvg_idx_df['Time_point'] = pd.Categorical(
+    hvg_idx_df['Time_point'], ['0h', '16h', '40h', '5d']
 )
 # ignore nan values
-degs_idx_df[~degs_idx_df['CD69'].isna()]['CD69'].plot(kind='hist', bins=100)
-plt.xlabel('rank of degs')
+hvg_idx_df[~hvg_idx_df['CD69'].isna()]['CD69'].plot(kind='hist', bins=100)
+plt.xlabel('rank of hvg')
 plt.ylabel('Counts')
 plt.savefig(
-    './res/tokenised_deg/deg_idx_histogram_CD69.pdf', dpi=300, bbox_inches='tight'
+    './res/tokenised_hvg/hvg_idx_histogram_CD69.pdf', dpi=300, bbox_inches='tight'
 )
 plt.close()
-sns.violinplot(data=degs_idx_df, y='CD69', hue='Time_point', orient='v')
+sns.violinplot(data=hvg_idx_df, y='CD69', hue='Time_point', orient='v')
 plt.xlabel('Timepoint')
 plt.ylabel('Ranks')
 plt.title('CD69')
-plt.savefig('./res/tokenised_deg/deg_idx_violin_CD69.pdf', dpi=300, bbox_inches='tight')
+plt.savefig('./res/tokenised_hvg/hvg_idx_violin_CD69.pdf', dpi=300, bbox_inches='tight')
 plt.close()
-degs_idx_df[~degs_idx_df['IL2RA'].isna()]['IL2RA'].plot(kind='hist', bins=100)
-plt.xlabel('rank of degs')
+hvg_idx_df[~hvg_idx_df['IL2RA'].isna()]['IL2RA'].plot(kind='hist', bins=100)
+plt.xlabel('rank of hvg')
 plt.ylabel('Counts')
 plt.title('IL2RA')
 plt.savefig(
-    './res/tokenised_deg/deg_idx_histogram_IL2RA.pdf', dpi=300, bbox_inches='tight'
+    './res/tokenised_hvg/hvg_idx_histogram_IL2RA.pdf', dpi=300, bbox_inches='tight'
 )
 plt.close()
-# plot violin plot of degs
-sns.violinplot(data=degs_idx_df, y='IL2RA', hue='Time_point', orient='v')
+# plot violin plot of hvgs
+sns.violinplot(data=hvg_idx_df, y='IL2RA', hue='Time_point', orient='v')
 plt.xlabel('Timepoint')
 plt.ylabel('Ranks')
 plt.title('IL2RA')
 plt.savefig(
-    './res/tokenised_deg/deg_idx_violin_IL2RA.pdf', dpi=300, bbox_inches='tight'
+    './res/tokenised_hvg/hvg_idx_violin_IL2RA.pdf', dpi=300, bbox_inches='tight'
 )
 plt.close()
-sns.violinplot(data=degs_idx_df, y='IL7R', hue='Time_point', orient='v')
+sns.violinplot(data=hvg_idx_df, y='IL7R', hue='Time_point', orient='v')
 plt.xlabel('Timepoint')
 plt.ylabel('Ranks')
 plt.title('IL7R')
-plt.savefig('./res/tokenised_deg/deg_idx_violin_IL7R.pdf', dpi=300, bbox_inches='tight')
+plt.savefig('./res/tokenised_hvg/hvg_idx_violin_IL7R.pdf', dpi=300, bbox_inches='tight')
 plt.close()
 # check for columns with only nan values
-nan_columns = degs_idx_df.columns[degs_idx_df.isna().all()].tolist()
+nan_columns = hvg_idx_df.columns[hvg_idx_df.isna().all()].tolist()
 print(f'Columns where all values are NaN: {nan_columns}')
+
+# calculate mean rank of hvgs
+mean_rank = hvg_idx_df.iloc[:, :-1].mean()
+# calculate mean expression of hvgs
+adata.var_names = adata.var['gene_name']
+expression = adata[:, hvg_idx_df.iloc[:, :-1].columns].X.A
+# calculate mean expression of hvgs
+median_expression = []
+non_zero = []
+for i in range(expression.shape[1]):
+    # non-zero median expression
+    tmp = expression[:, i]
+    tmp_ = tmp[tmp != 0]
+    median_expression.append(np.median(tmp_))
+    # count number of non-zero values
+    non_zero.append(len(tmp_))
+
+mean_df = pd.DataFrame(
+    {'Mean_rank': mean_rank, 'Mean_expression': median_expression, 'Non_zero': non_zero}
+)
+# create spearman correltion scatter plot
+fig = plt.figure()
+ax = fig.add_subplot(111)
+sc = ax.scatter(
+    mean_df['Mean_rank'], mean_df['Mean_expression'], s=5, c=mean_df['Non_zero']
+)
+plt.colorbar(sc)
+plt.xlabel('Mean rank')
+plt.ylabel('Non-zero median expression')
+plt.savefig(
+    './res/tokenised_hvg/mean_rank_vs_median_expression.pdf',
+    dpi=300,
+    bbox_inches='tight',
+)
+plt.close()

T_perturb/val.py
@@ -29,25 +29,25 @@ def get_args():
     parser.add_argument(
         '--test_mode',
         type=str,
-        default='masking',
+        default='count',
         help='Mode [masking, count]',
     )
     parser.add_argument(
         '--split',
         type=bool,
-        default=False,
+        default=True,
         help='split data for extrapolation',
     )
     parser.add_argument(
         '--generate',
         type=bool,
-        default=False,
+        default=True,
         help='generate data',
     )
     parser.add_argument(
         '--return_embeddings',
         type=bool,
-        default=True,
+        default=False,
         help='return embedding',
     )
     parser.add_argument(
@@ -59,10 +59,10 @@ def get_args():
     parser.add_argument(
         '--ckpt_masking_path',
         type=str,
-        default='/lustre/scratch123/hgi/projects/healthy_imm_expr/t_generative/'
-        'T_perturb/T_perturb/Model/checkpoints/'
-        '20240322_1802_petra_train_masking_lr_0.001_'
-        'wd_0.0_batch_128_mlmp_0.3_tp_1-2-3.ckpt',
+        default='/lustre/scratch123/hgi/projects/healthy_imm_expr/t_generative'
+        '/T_perturb/T_perturb/Model/checkpoints/'
+        '20240326_1522_petra_train_masking_lr_0.001_'
+        'wd_0.001_batch_64_mlmp_0.15_tp_1-2-3.ckpt',
         help='path to checkpoint',
     )
     parser.add_argument(
@@ -70,8 +70,8 @@ def get_args():
         type=str,
         default='/lustre/scratch123/hgi/projects/healthy_imm_expr/t_generative/'
         'T_perturb/T_perturb/Model/checkpoints/'
-        '20240322_1900_petra_train_count_lr_0.0005_'
-        'wd_0.001_batch_128_zinb_tp_1-2-3.ckpt',
+        '20240326_1730_petra_train_count_lr_0.0005_'
+        'wd_0.001_batch_64_zinb_tp_1-2-3.ckpt',
         help='path to checkpoint',
     )
     parser.add_argument(
@@ -105,7 +105,7 @@ def get_args():
         type=str,
         default=(
             '/lustre/scratch123/hgi/projects/healthy_imm_expr/t_generative/T_perturb/'
-            'T_perturb/pp/res/h5ad_pairing_hvg_tgt'
+            'T_perturb/pp/res/h5ad_pairing_hvg_tgt/'
         ),
         help='path to tgt',
     )
@@ -115,10 +115,10 @@ def get_args():
         '--log_dir', type=str, default='logs', help='path to data directory'
     )
     parser.add_argument(
-        '--mlm_probability', type=float, default=0.5, help='mlm probability'
+        '--mlm_probability', type=float, default=0.15, help='mlm probability'
     )
-    parser.add_argument('--max_len', type=int, default=246, help='max sequence length')
-    parser.add_argument('--petra_lr', type=float, default=1e-3, help='learning rate')
+    parser.add_argument('--max_len', type=int, default=400, help='max sequence length')
+    parser.add_argument('--petra_lr', type=float, default=0.001, help='learning rate')
     parser.add_argument('--count_lr', type=float, default=0.0005, help='learning rate')
     parser.add_argument('--petra_wd', type=float, default=0.001, help='weight decay')
     parser.add_argument('--count_wd', type=float, default=0.001, help='weight decay')
@@ -275,7 +275,6 @@ def main() -> None:
         )
         conditions_combined = torch.tensor(conditions_combined, dtype=torch.long)
     print('Data loaded and preprocessed.')
-    print(tgt_adata_tmp.var['gene_name'])
     # Initialize model module
     # ----------------------------------------------------------------------------------
     if args.test_mode == 'masking':