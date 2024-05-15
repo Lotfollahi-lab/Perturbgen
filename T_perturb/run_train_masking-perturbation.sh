b6a74c49599f67e6cb5c7f19772ce6b1c44e3411
Irene Bonafonte Pardàs <irene.bonafonte@hpc-build01.scidom.de>
2024-05-15 16:17:26 +0200

    runs

--- T_perturb/run_train_masking-perturbation.sh
+++ T_perturb/run_train_masking-perturbation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -J 5_train_masking_Norman2019
-#SBATCH -o ../../logs/5_Norman2019_train_masking.out
-#SBATCH -e ../../logs/5_Norman2019_train_masking.err
+#SBATCH -J 8_train_masking_Norman2019
+#SBATCH -o ../../logs/8_Norman2019_train_masking.out
+#SBATCH -e ../../logs/8_Norman2019_train_masking.err
 #SBATCH -t 24:00:00
 #SBATCH -p gpu_p
 #SBATCH --gres=gpu:1
@@ -28,18 +28,19 @@ python3 $cwd/train.py \
 --num_cells 0 \
 --split True \
 --splitting_mode gears-simulation \
---src_dataset_folder ../../datasets/Norman2019/dataset/filtered_tokenised_hvg_pairing_gene2vecpert_control.dataset/ \
+--src_dataset_folder ../../datasets/Norman2019/dataset/filtered_tokenised_hvg_pairing_GFpert_control.dataset/ \
 --tgt_dataset_folder ../../datasets/Norman2019/dataset/filtered_tokenised_hvg_pairing_perturbed.dataset \
 --src_adata_folder ../../datasets/Norman2019/adata/filtered_tokenised_hvg_pairing_control.h5ad \
 --tgt_adata_folder ../../datasets/Norman2019/adata/filtered_tokenised_hvg_pairing_perturbed.h5ad \
---batch_size 32 \
+--batch_size 16 \
 --epochs 50 \
 --max_len 1750 \
 --petra_lr 0.001 \
---petra_wd 0.001 \
---count_wd 0.001 \
+--petra_wd 0.0001 \
 --mlm_probability 0.3 \
 --n_workers 20 \
+--num_layers 5 \
+--d_ff 16 \
 --seed 1 \
 --base_path /lustre/groups/imm01/workspace/irene.bonafonte/
 echo "--- Finished computing model"
