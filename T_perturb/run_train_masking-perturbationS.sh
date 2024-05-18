c28bfc2868ff2590ddf353db5835cdfdf4a1b0d8
Irene Bonafonte <ip14@sanger.ac.uk>
2024-05-18 20:55:21 +0100

    experiment

--- T_perturb/run_train_masking-perturbationS.sh
+++ T_perturb/run_train_masking-perturbationS.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #BSUB -q gpu-lotfollahi # name of the partition to run job on (options: gpu-normal, gpu-huge, gpu-lotfollahi)
-#BSUB -gpu 'mode=exclusive_process:num=4' # request for exclusive access to gpu
+#BSUB -gpu 'mode=exclusive_process:num=2' # request for exclusive access to gpu
 #BSUB -n 16 # number of cores
 #BSUB -G team361 # groupname for billing
 #BSUB -cwd /lustre/scratch126/cellgen/team361/ip14/Projects/2024Mar_Tperturb/T_perturb/T_perturb/ # working directory
@@ -8,7 +8,7 @@
 #BSUB -e ../../logs/%J.err # error file
 #BSUB -M 40GB  # RAM memory part 2. Default: 100MB
 #BSUB -R 'select[mem>40GB] rusage[mem=40GB]' # RAM memory part 1. Default: 100MB
-#BSUB -J 8_gfTune # job name
+#BSUB -J 10_gfTune # job name
 
 # activate conda environment
 source ~/.bashrc
@@ -31,22 +31,21 @@ python3 $cwd/train.py \
 --train_mode masking \
 --split True \
 --splitting_mode gears-simulation \
---ckpt_file 20240515_1932_petra_mode_masking_lr_0.001_wd_0.0001_batch_16_mlmp_0.3_seed1_hvg_pairing_GFpert.ckpt \
 --src_dataset_folder ../../datasets/Norman2019/dataset/filtered_tokenised_hvg_pairing_GFpert_control.dataset \
 --tgt_dataset_folder ../../datasets/Norman2019/dataset/filtered_tokenised_hvg_pairing_perturbed.dataset \
 --src_adata_folder ../../datasets/Norman2019/adata/filtered_tokenised_hvg_pairing_control.h5ad \
 --tgt_adata_folder ../../datasets/Norman2019/adata/filtered_tokenised_hvg_pairing_perturbed.h5ad \
---batch_size 16 \
+--batch_size 45 \
 --epochs 50 \
 --max_len 1750 \
 --petra_lr 0.001 \
 --petra_wd 0.0001 \
 --num_layers 5 \
 --d_ff 16 \
---mlm_probability 0.3 \
+--mlm_probability 0.5 \
 --n_workers 16 \
 --seed 1 \
 --base_path /lustre/scratch126/cellgen/team361/ip14 \
---tune_geneformer True \
---retrain_masking True
+--tune_geneformer False \
+--perturbation_encoding_mode mlp compress_src
 echo "--- Finished computing model"
