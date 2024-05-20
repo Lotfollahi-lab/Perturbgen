f9fc67d15df2de2518a60afe1d612905cf17db68
Irene Bonafonte Pardàs <irene.bonafonte@hpc-build01.scidom.de>
2024-05-21 00:44:06 +0200

    sweep

--- T_perturb/run_sweep-perturbationS.sh
+++ T_perturb/run_sweep-perturbationS.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #BSUB -q gpu-lotfollahi # name of the partition to run job on (options: gpu-normal, gpu-huge, gpu-lotfollahi)
-#BSUB -gpu 'mode=exclusive_process:num=3' # request for exclusive access to gpu
-#BSUB -n 32 # number of cores
+#BSUB -gpu 'mode=exclusive_process:num=8:block=yes' # request for exclusive access to gpu
+#BSUB -n 16 # number of cores
 #BSUB -G team361 # groupname for billing
 #BSUB -cwd /lustre/scratch126/cellgen/team361/ip14/Projects/2024Mar_Tperturb/T_perturb/T_perturb/ # working directory
 #BSUB -o ../../logs/sweep_%J.out # output file
 #BSUB -e ../../logs/sweep_%J.err # error file
-#BSUB -M 64GB  # RAM memory part 2. Default: 100MB
-#BSUB -R 'select[mem>64GB] rusage[mem=64GB]' # RAM memory part 1. Default: 100MB
-#BSUB -J 4.1_train_mask_sweep_Norman2019 # job name
+#BSUB -M 40GB  # RAM memory part 2. Default: 100MB
+#BSUB -R 'select[mem>40GB] rusage[mem=40GB]' # RAM memory part 1. Default: 100MB
+#BSUB -J maskSweep # job name
 
 source ~/.bashrc
 conda activate Tperturb
