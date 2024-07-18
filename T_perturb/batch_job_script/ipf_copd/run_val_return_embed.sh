04f0e1d1e19fc21c3e5b76d487e975ad400ed9c9
kchl5 <k.ly@hotmail.ch>
2024-07-18 13:27:31 +0100

    adopt test_step for disease pre-training

--- T_perturb/batch_job_script/ipf_copd/run_val_return_embed.sh
+++ T_perturb/batch_job_script/ipf_copd/run_val_return_embed.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #BSUB -q gpu-lotfollahi # name of the partition to run job on (options: gpu-normal, gpu-huge, gpu-lotfollahi)
-#BSUB -gpu 'mode=exclusive_process:num=2:block=yes' # request for exclusive access to gpu
+#BSUB -gpu 'mode=exclusive_process:num=1:block=yes' # request for exclusive access to gpu
 #BSUB -n 16 # number of cores
 #BSUB -G teamtrynka # groupname for billing
 #BSUB -cwd /lustre/scratch123/hgi/projects/healthy_imm_expr/t_generative/T_perturb/T_perturb # working directory
-#BSUB -o logs/ipf_copd_masking_%J.out # output file
-#BSUB -e logs/ipf_copd_masking_%J.err # error file
+#BSUB -o logs/ipf_copd_embed_%J.out # output file
+#BSUB -e logs/ipf_copd_embed_%J.err # error file
 #BSUB -M 50000  # RAM memory part 2. Default: 100MB
 #BSUB -R 'span[ptile=16]'  # Allocate 4 CPU cores per node
 #BSUB -R 'select[mem>50000] rusage[mem=50000]' # RAM memory part 1. Default: 100MB
-#BSUB -J ipf_copd_masking # job name
+#BSUB -J ipf_copd_embed # job name
+
 
 # load cuda
 module load cuda-12.1.1
@@ -18,35 +19,38 @@ module load cuda-12.1.1
 source /lustre/scratch123/hgi/projects/healthy_imm_expr/t_generative/.petra_cuda12/bin/activate
 cwd=$(pwd)
 
-export WANDB_DIR=$cwd/wandb
+# export WANDB_DIR=$cwd/wandb
 # run script
 echo "--- Start computing model"
 
-# Interpolation
-# python3 $cwd/train.py \
-python3 /lustre/scratch123/hgi/projects/healthy_imm_expr/t_generative/T_perturb/T_perturb/train.py \
---train_mode masking \
+# # Run python script for rna
+# python3 $cwd/val.py \
+python3 /lustre/scratch123/hgi/projects/healthy_imm_expr/t_generative/T_perturb/T_perturb/val.py \
+--test_mode masking \
 --split False \
 --splitting_mode random \
+--return_embed True \
+--generate False \
+--ckpt_masking_path "./T_perturb/T_perturb/Model/checkpoints/20240717_0924_ipf_copd_attn_masking_lr_0.0001_wd_0.0001_batch_16_mlmp_0.15_tp_1_s_42-epoch=19.ckpt" \
 --output_dir "./T_perturb/T_perturb/plt/res/ipf_copd" \
 --src_dataset "./CellGen-reproducibility/ipf_copd/processed_data/dataset_hvg_src/Control.dataset" \
 --tgt_dataset_folder "./CellGen-reproducibility/ipf_copd/processed_data/dataset_hvg_tgt/" \
 --src_adata "./CellGen-reproducibility/ipf_copd/processed_data/h5ad_pairing_hvg_src/Control.h5ad" \
 --tgt_adata_folder "./CellGen-reproducibility/ipf_copd/processed_data/h5ad_pairing_hvg_tgt" \
 --cell_pairing_dir "./CellGen-reproducibility/ipf_copd/processed_data/cell_pairing" \
+--tokenid_to_genename_dict  "./CellGen-reproducibility/ipf_copd/tokenid_to_genename.pkl" \
 --batch_size 16 \
 --max_len 1650 \
---epochs 20 \
 --tgt_vocab_size 25426 \
 --cellgen_lr 0.0001 \
 --cellgen_wd 0.0001 \
---mlm_prob 0.15 \
+--count_lr 0.00005 \
+--count_wd 0.01 \
 --n_workers 32 \
 --d_ff 128 \
 --num_layers 1 \
 --time_steps 1 \
 --var_list CellType_Category Manuscript_Identity Subclass_Cell_Identity Celltype_HLCA disease IPF_signature IPF_signature_disease profibrotic_mac_signature \
 --encoder_type GF_frozen \
---moe_type moe_ffn \
---alpha 0.8
+--moe_type none
 echo "--- Finished computing model"
