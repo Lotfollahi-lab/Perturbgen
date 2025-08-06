9cddd23b00f78e6064d5a86343e5a9905cea53f3
Delshad Vaghari <47316091+delshadv@users.noreply.github.com>
2025-08-06 16:35:48 +0100

    Update and rename 3_new.sh to 3_perturb.sh

---
+++ cytomeister/batch_job_script/lps/3_perturb.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#BSUB -q gpu-lotfollahi # name of the partition to run job on (options: gpu-normal, gpu-huge, gpu-lotfollahi)
+#BSUB -gpu "mode=shared:num=1" # request for exclusive access to gpu  :gmodel=NVIDIA_H100_HBM3_80GB
+#BSUB -n 8 # number of cores
+#BSUB -G team361 # groupname for billing
+#BSUB -cwd /lustre/scratch126/cellgen/lotfollahi/dv8/trace/T_perturb # working directory
+#BSUB -o T_perturb/log/perturb_cluster_prkar2b_%J.out # output file
+#BSUB -e T_perturb/log/perturb_cluster_prkar2b_%J.err # error file
+#BSUB -M 50000  # RAM memory part 2. Default: 100MB
+#BSUB -R 'select[mem>50000] rusage[mem=50000]' # RAM memory part 1. Default: 100MB
+#BSUB -J hspc_perturb_cluster_prkar2b # job name
+
+# load cuda
+module load cuda-12.1.1
+
+# activate pyenv
+# source /lustre/scratch126/cellgen/team361/kl11/t_generative/.cellgen_4096/bin/activate
+cwd=$(pwd)
+
+export WANDB_DIR=$cwd/wandb
+# run script
+echo "--- Start computing model"
+
+# python3 $cwd/train.py \
+python3 /lustre/scratch126/cellgen/lotfollahi/dv8/trace/T_perturb/T_perturb/Perturb/val.py \
+--config /lustre/scratch126/cellgen/lotfollahi/dv8/trace/T_perturb/T_perturb/configs/eval/LPS/IL1B_KO1.yaml
+echo "--- Completed perturbation"
