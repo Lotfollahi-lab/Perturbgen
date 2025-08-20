7fcfb8e472f206ab9f09dd60472d3afb5196c209
Dikshita Nandanwar <dn10@sanger.ac.uk>
2025-08-20 17:38:22 +0100

    minor fix

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'cytomeister'
+project = 'perturbgen'
 copyright = '2025, Dikshita'
 author = 'Dikshita'
 
